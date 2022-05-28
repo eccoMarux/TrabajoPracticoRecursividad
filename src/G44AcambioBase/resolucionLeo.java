40842085409e0e37c5c40724f10ede3c44fbd3e5
LeoBruno400 <leobruno400@gmail.com>
2022-05-28 20:07:21 -0300

    resolucion completa

--- src/G44AcambioBase/resolucionLeo.java
+++ src/G44AcambioBase/resolucionLeo.java
@@ -2,6 +2,22 @@ package G44AcambioBase;
 
 public class resolucionLeo {
     public static void main(String[] args) {
-        int numero = 45;
+        int numero = 5;
+        System.out.println(decimalABinario(numero));
+    }
+
+    public static String decimalABinario(int num){
+        String cadenaBinario = "";
+        int restoNum;
+        int cociente;
+
+        if(num<2){
+            cadenaBinario = "1";
+        }else{
+            restoNum = num%2;
+            cociente = num/2;
+            cadenaBinario += decimalABinario(cociente) + restoNum;
+        }
+        return cadenaBinario;
     }
 }
