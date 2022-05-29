90e5f00115ba0ab64400e6b7b53d6416542317b6
jeremiasHerr <jherrera.nqn@gmail.com>
2022-05-29 20:32:01 -0300

    Agregar ejercicio 1d

---
+++ src/G21DEnteros/resolucionJere.java
@@ -0,0 +1,32 @@
+package G21DEnteros;
+
+import java.util.Scanner;
+
+public class resolucionJere {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arreglo = {1,2,1,2,2,12,5};
+        int n;
+        System.out.println("Ingrese el valor de N");
+        n = sc.nextInt();
+        System.out.println(elemMenorValor(arreglo, n, 0));
+        sc.close();
+    }
+
+    public static int elemMenorValor (int [] arreglo, int n, int posicion){
+        int resultado;
+        if(posicion!=arreglo.length-1){
+            resultado = elemMenorValor(arreglo, n, posicion+1);
+            if((arreglo[posicion]<resultado || resultado==-1) && arreglo[posicion]>=n){
+                resultado = arreglo[posicion];
+            }
+        } else {
+            if(arreglo[posicion]>=n){
+                resultado = arreglo[posicion];
+            } else {
+                resultado = -1;
+            }
+        }
+        return resultado;
+    }
+}
