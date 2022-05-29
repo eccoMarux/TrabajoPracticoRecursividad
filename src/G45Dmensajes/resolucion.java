f911f2232bcc4382e9f5ed8543ab5d3cfcd72b5a
LeoBruno400 <leobruno400@gmail.com>
2022-05-29 20:12:26 -0300

    Co-authored-by: Maru <eccoMarux@users.noreply.github.com>

--- src/G45Dmensajes/resolucion.java
+++ src/G45Dmensajes/resolucion.java
@@ -4,12 +4,13 @@ public class resolucion {
     public static void main(String[] args) {
         // 9, 28, 14, 7, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2 , 1
         int numero = 9;
-        mensajes(numero);
 
+        System.out.println();
+        mensajes(numero);
+        System.out.println("\nLa suma de los valores da como resultado " + suma(numero) + " mensajes enviados.\n");
     }
 
-    public static int mensajes(int num) {
-        
+    public static void mensajes(int num) {
         System.out.print(num + " ");
         if (num != 1) {
             if (num % 2 == 0) {
@@ -18,6 +19,19 @@ public class resolucion {
                 mensajes((num * 3) + 1);
             }
         }
-        return 
+    }
+
+    public static int suma(int num) {
+        int valores;
+        if (num == 1) {
+            valores = 1;
+        } else {
+            if (num % 2 == 0) {
+                valores = num + suma(num / 2);
+            } else {
+                valores = num + suma((num * 3) + 1);
+            }
+        }
+        return valores;
     }
 }
