c8c26bf8186f2db4d6d5cef1aac025469284c576
LeoBruno400 <leobruno400@gmail.com>
2022-05-29 22:27:56 -0300

    resolucionLeo comentarios

--- src/G44AcambioBase/resolucionLeo.java
+++ src/G44AcambioBase/resolucionLeo.java
@@ -7,15 +7,20 @@ public class resolucionLeo {
     }
 
     public static String decimalABinario(int num){
+        //Para convertir un numero decimal a binario:
+        //-Tomamos los restos de las divisones sucesivas de un numero X
+        //-Una vez hechas estas divisones con sus respectivos restos, se anotan los mismos de atras hacia adelante
         String cadenaBinario = "";
         int restoNum;
         int cociente;
 
-        if(num<2){
+        if(num<2){ //Mientras el numero sea menor a 2 se podria seguir dividiendo 
             cadenaBinario = "1";
         }else{
             restoNum = num%2;
+               //El resto de la divison del numero que se iria guardando en una cadena de texto en el apilado 
             cociente = num/2;
+                //Guardo el cociente del numero para que entre en la recursividad
             cadenaBinario += decimalABinario(cociente) + restoNum;
         }
         return cadenaBinario;
