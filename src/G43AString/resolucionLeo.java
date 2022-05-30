205e0926e3fadec307b6a4ee64e0561e165aff58
LeoBruno400 <leobruno400@gmail.com>
2022-05-29 22:02:13 -0300

    resolucionLeo

---
+++ src/G43AString/resolucionLeo.java
@@ -0,0 +1,35 @@
+package G43AString;
+
+import java.util.Scanner;
+
+public class resolucionLeo {
+    public static void main(String[] args) {
+        // Verifica si una palabra palindromo
+        Scanner sc = new Scanner(System.in);
+        String palabra;
+        System.out.println("Ingrese una palabra");
+        palabra = sc.nextLine();
+        palabra = palabra.replaceAll(" ", "");
+        palabra = palabra.toLowerCase();
+
+        int der = palabra.length() - 1;
+        int izq = 0;
+        
+        System.out.println(verificacionPalabra(palabra, izq, der));
+        sc.close();
+    }
+
+    public static boolean verificacionPalabra(String palabra, int izq, int der) {
+        boolean similar = true;
+
+        if (izq < der) { // Caso iterativo: mientras la izq sea menor a la der
+            if (palabra.charAt(izq) == palabra.charAt(der)) { // Se compara letra por letra de los extremos hacia el
+                                                              // centro
+                similar = verificacionPalabra(palabra, izq + 1, der - 1); // Se repite hacia dentro
+            } else {
+                similar = false; // Caso base: de no encontrar la similitud en al menos 1 letra retorna false
+            }
+        }
+        return similar;
+    }
+}
